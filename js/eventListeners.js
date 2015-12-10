88983b0abb2798eef1fac123130e75e7a808b621
torsten92 <torstengustavsson1992@gmail.com>
2015-12-10 18:01:27 +0100

    test touch 3

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -84,82 +84,40 @@ function onWindowResize( event ) {
 }
 
 
-var ongoingTouches = new Array();
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 
-function handleStart(evt) {
-  evt.preventDefault();
+function onDocumentTouchStart( event ) {
+	if ( event.touches.length === 1 ) {
+		event.preventDefault();
 
-  var ctx = window.getContext("2d");
-  var touches = evt.changedTouches;
-        
-  for (var i = 0; i < touches.length; i++) {
-    ongoingTouches.push(copyTouch(touches[i]));
-    var color = colorForTouch(touches[i]);
-    ctx.beginPath();
-    ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
-    ctx.fillStyle = color;
-    ctx.fill();
-  }
-}
+		mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+		targetRotationOnMouseDown = targetRotation;
 
-function handleMove(evt) {
-  evt.preventDefault();
-  var ctx = window.getContext("2d");
-  var touches = evt.changedTouches;
-
-  for (var i = 0; i < touches.length; i++) {
-    var color = colorForTouch(touches[i]);
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-
-    if (idx >= 0) {
-      ctx.beginPath();
-      log("ctx.moveTo(" + ongoingTouches[idx].pageX + ", " + ongoingTouches[idx].pageY + ");");
-      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-      log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
-      ctx.lineTo(touches[i].pageX, touches[i].pageY);
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = color;
-      ctx.stroke();
-
-      ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-      log(".");
-    }
-  }
-}
-
-function handleEnd(evt) {
-  evt.preventDefault();
-  theTree.iterate();
-  var ctx = window.getContext("2d");
-  var touches = evt.changedTouches;
-
-  for (var i = 0; i < touches.length; i++) {
-    var color = colorForTouch(touches[i]);
-    var idx = ongoingTouchIndexById(touches[i].identifier);
-
-    if (idx >= 0) {
-      ctx.lineWidth = 4;
-      ctx.fillStyle = color;
-      ctx.beginPath();
-      ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-      ctx.lineTo(touches[i].pageX, touches[i].pageY);
-      ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
-      ongoingTouches.splice(idx, 1);  // remove it; we're done
-    } else {
-      log("can't figure out which touch to end");
-    }
-  }
-}
-
-function handleCancel(evt) {
-  evt.preventDefault();
-  log("touchcancel.");
-  var touches = evt.changedTouches;
-  
-  for (var i = 0; i < touches.length; i++) {
-    ongoingTouches.splice(i, 1);  // remove it; we're done
-  }
+	}
 }
 
 
-//Fanden fløjte mig drenge, bør dette fejres fandeme!
+function onDocumentTouchMove( event ) {
+	if ( event.touches.length === 1 ) {
+		event.preventDefault();
+
+		onMouseDownPosition.x = event.touches[ 0 ].pageX - windowHalfX;
+		onMouseDownPosition.y = event.touches[ 0 ].pageY - windowHalfY;
+		targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+		
+		camTheta = - ( ( event.clientX - onMouseDownPosition.x ) * 0.5 ) + onMouseDownTheta;
+		camPhi = ( ( event.clientY - onMouseDownPosition.y ) * 0.5 ) + onMouseDownPhi;
+
+		camPhi = Math.min( 90, Math.max( 0, camPhi ) );
+		
+		camera.rotation.y = camTheta * Math.PI / 180;
+		camera.rotation.x = -camPhi * Math.PI / 180;
+		
+		camera.position.x = camRadius * Math.sin( camTheta * Math.PI / 180 ) * Math.cos( camPhi * Math.PI / 180 );
+		camera.position.y = camRadius * Math.sin( camPhi * Math.PI / 180 );
+		camera.position.z = camRadius * Math.cos( camTheta * Math.PI / 180 ) * Math.cos( camPhi * Math.PI / 180 );
+		
+		camera.updateMatrix();
+	}
+}
